4e3dbb9b4a9a269afd6ef7f2291ff5311481fdfe
Jhonatan Rodrigues <jhon.devbr@gmail.com>
2025-12-01 22:38:11 -0300

    Criadas as classes Record e Seller

---
+++ Desafio01/Desafio01/Entities/Record.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Desafio01.Entities
+{
+    public class Record
+    {
+        [JsonPropertyName("vendas")]
+        public List<Seller> Sellers = new List<Seller>();
+    }
+}
---
+++ Desafio01/Desafio01/Entities/Seller.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Desafio01.Entities
+{
+    public class Seller
+    {
+        [JsonPropertyName("vendedor")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("valor")]
+        public decimal Value { get; set; }
+
+        public Seller(string name, decimal value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
