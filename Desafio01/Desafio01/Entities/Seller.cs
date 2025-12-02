0388bf9a9c9b34a3d4a88ae19fc54fa326de2d9f
Jhonatan Rodrigues <jhon.devbr@gmail.com>
2025-12-01 22:49:18 -0300

    Criada a interface de CommissionService, e sua implementação

--- Desafio01/Desafio01/Entities/Seller.cs
+++ Desafio01/Desafio01/Entities/Seller.cs
@@ -1,8 +1,10 @@
-﻿using System.Text.Json.Serialization;
+﻿using Desafio01.Services;
+using System.Globalization;
+using System.Text.Json.Serialization;
 
 namespace Desafio01.Entities
 {
-    public class Seller
+    public class Seller : CommissionService
     {
         [JsonPropertyName("vendedor")]
         public string Name { get; set; }
@@ -14,6 +16,24 @@ namespace Desafio01.Entities
         {
             Name = name;
             Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"> Vendedor: {Name}\n" +
+                   $"> Valor da comição: {Value.ToString("C2", new CultureInfo("pt-BR"))}\n";
+        }
+
+        public decimal CommissionRate(decimal amount)
+        {
+            if (amount < 500.0m)
+            {
+                return amount * 0.01m;
+            }
+            else
+            {
+                return amount * 0.05m;
+            }
         }
     }
 }
---
+++ Desafio01/Desafio01/Services/CommissionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio01.Services
+{
+    public interface CommissionService
+    {
+        public decimal CommissionRate(decimal amount);
+    }
+}
