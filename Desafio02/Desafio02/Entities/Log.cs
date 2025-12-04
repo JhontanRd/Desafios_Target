3e308485c16bb9edbc3d4708fe05b79dbd444ad8
Jhonatan Rodrigues <jhon.devbr@gmail.com>
2025-12-03 22:21:07 -0300

    Criadas as classes de Stock e Log

---
+++ Desafio02/Desafio02/Entities/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio02.Entities
+{
+    public class Log
+    {
+        private int _id;
+        private string _logDescription;
+        private DateTime _created;
+
+        public Log(int id, string logDescription, DateTime created)
+        {
+            _id = id;
+            _logDescription = logDescription;
+            _created = created;
+        }
+
+    }
+}
---
+++ Desafio02/Desafio02/Entities/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio02.Entities
+{
+    public class Stock
+    {
+        public SortedSet<Product> Stocks { get; set; } = new SortedSet<Product>();
+        private List<Log> _operationsLog = new List<Log>();
+
+        public Product TakeProductForCode(int code)
+        {
+            return Stocks.FirstOrDefault(p => p.Code == code);
+        }
+
+
+    }
+}
